322aeefb284d19c8edecf5ea584014a22106312d
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-15 16:08:38 +0200

    feat: establish MySQL connection and set up Express server with CORS

--- data/db.js
+++ data/db.js
@@ -10,4 +10,10 @@ const credentials = {
 
 const connection = mysql.createConnection(credentials)
 
+connection.connect((err) => {
+    if (err) throw err
+    console.log('Connected to MySQL database');
+
+})
+
 module.exports = connection
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.listen(port, () => {
+    console.log('Server is listening on http://localhost' + port);
+
+})
+
+app.use(express.json());
+
+app.use(cors({
+    origin: ''
+}))
