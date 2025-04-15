8a82a83164a79fe9fc4b1f5cbd4be3ec4e83ebc0
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-15 16:34:49 +0200

    fix: correct PORT variable usage and enhance route definitions for movies API

--- server.js
+++ server.js
@@ -1,15 +1,58 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+const PORT = 3000
 
-app.listen(port, () => {
-    console.log('Server is listening on http://localhost' + port);
+app.listen(PORT, () => {
+    console.log('Server is listening on http://localhost:' + PORT);
 
 })
 
 app.use(express.json());
 
+
+// There is the front-end URL
 app.use(cors({
     origin: ''
 }))
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.send('Movies API Server!')
+})
+
+// Index route for movies
+
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM movies'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({
+            error: 'Query failed'
+        })
+        console.log(results);
+        res.json(results)
+
+    })
+    //  res.json({ message: 'List of movies' })
+})
+
+// Show route for a single movie
+
+app.get('/:id', (req, res) => {
+    const movieId = Number(req.params.id)
+
+    const sql = 'SELECT * FROM movies where id = ?'
+
+    connection.query(sql, [movieId], (err, results) => {
+        if (err) return res.status(500).json({
+            error: 'Query failed'
+        })
+        if (results.length === 0) return res.status(404).json({
+            message: 'There is nothing to show'
+        })
+        const movie = results[0]
+
+        res.json(movie)
+    })
+})
